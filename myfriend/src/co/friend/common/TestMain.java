ccc967c9c97a5915aec865c14f155f1dadf7826b
mxvrun13 <mxvrun13@naver.com>
2021-06-09 20:59:55 +0900

    이메일 성공 (active jar 넣기)

--- myfriend/src/co/friend/common/TestMain.java
+++ myfriend/src/co/friend/common/TestMain.java
@@ -19,7 +19,7 @@ public class TestMain {
 		Properties props = new Properties(); 
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", host); 
-		props.put("mail.smtp.port", 465); 
+		props.put("mail.smtp.port", 587); 
 		props.put("mail.smtp.auth", "true");
 		
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
